f04d5d55ce6d339cbd67b94a2006d094cd07fcf4
Neeraj kumar <neerajishit@gmail.com>
2022-03-08 11:23:55 +0530

    🐛 fixed bitsearch.to

--- torrents/bitsearch.py
+++ torrents/bitsearch.py
@@ -20,35 +20,36 @@ class Bitsearch:
                 my_dict = {
                     'data': []
                 }
-                for divs in soup.find_all('div', class_='search-result'):
+                for divs in soup.find_all('li', class_='search-result'):
                     info = divs.find("div", class_='info')
                     name = info.find('h5', class_='title').find('a').text
                     url = info.find('h5', class_='title').find('a')['href']
                     category = info.find('div').find(
                         'a', class_='category').text
                     stats = info.find('div', class_='stats').find_all('div')
-                    downloads = stats[0].text
-                    size = stats[1].text
-                    seeders = stats[2].text.strip()
-                    leechers = stats[3].text.strip()
-                    date = stats[4].text
-                    links = divs.find("div", class_='links').find_all('a')
-                    magnet = links[1]['href']
-                    torrent = links[0]['href']
-                    my_dict['data'].append({
-                        'name': name,
-                        'size': size,
-                        'seeders': seeders,
-                        'leechers': leechers,
-                        'category': category,
-                        'hash': re.search(r'([{a-f\d,A-F\d}]{32,40})\b', magnet).group(0),
-                        'magnet': magnet,
-                        'torrent': torrent,
-                        'url': self.BASE_URL + url,
-                        'date': date,
-                        'downloads': downloads
+                    if stats:
+                        downloads = stats[0].text
+                        size = stats[1].text
+                        seeders = stats[2].text.strip()
+                        leechers = stats[3].text.strip()
+                        date = stats[4].text
+                        links = divs.find("div", class_='links').find_all('a')
+                        magnet = links[1]['href']
+                        torrent = links[0]['href']
+                        my_dict['data'].append({
+                            'name': name,
+                            'size': size,
+                            'seeders': seeders,
+                            'leechers': leechers,
+                            'category': category,
+                            'hash': re.search(r'([{a-f\d,A-F\d}]{32,40})\b', magnet).group(0),
+                            'magnet': magnet,
+                            'torrent': torrent,
+                            'url': self.BASE_URL + url,
+                            'date': date,
+                            'downloads': downloads
 
-                    })
+                        })
                 try:
                     total_pages = int(soup.select(
                         'body > main > div.container.mt-2 > div > div:nth-child(1) > div > span > b')[0].text) / 20  # !20 search result available on each page
